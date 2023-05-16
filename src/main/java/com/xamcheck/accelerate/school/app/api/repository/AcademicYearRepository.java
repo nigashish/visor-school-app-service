d7bf366cb769b9f531de2a07d46b5a3d15f46186
nigashish <ashishn@eupheus.in>
2023-05-16 13:35:45 +0530

    Add model file into a seperate service

--- src/main/java/com/xamcheck/accelerate/school/app/api/repository/AcademicYearRepository.java
+++ src/main/java/com/xamcheck/accelerate/school/app/api/repository/AcademicYearRepository.java
@@ -2,10 +2,9 @@ package com.xamcheck.accelerate.school.app.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-//import com.accelerate.visor.model.AcademicYear;
+import com.accelerate.visor.model.AcademicYear;
 @Repository
-public interface AcademicYearRepository  {
-	//extends JpaRepository<AcademicYear, Long>
+public interface AcademicYearRepository extends JpaRepository<AcademicYear, Long> {
+	//
 
 }
